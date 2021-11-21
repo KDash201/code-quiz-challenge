dfe6caae1e98a275320ba64d8a2715a166eb8223
KDash201 <KVonMadison@Yahoo.com>
2021-11-21 16:50:32 -0500

    Created questions in JS

--- assets/css/script.js
+++ assets/css/script.js
@@ -7,9 +7,32 @@ const containStartEl = document.getElementById("contain-start");
 const questionsEl = document.getElementById("questions");
 const choicesEl = document.getElementById("choices");
 
+let askQuestions = [
+  {
+    question:
+      "Which attribute requires you to put a hashtag before the name when deeclaring it in CSS?",
+    choiceA: "Correct",
+    choiceB: "Wrong",
+    choiceC: "Wrong",
+    correct: "A",
+  },{
+  question:
+    "How many HTML heading tags are there?",
+  choiceA: "Wrong",
+  choiceB: "Correct",
+  choiceC: "Wrong",
+  correct: "B",
+},{
+  question:
+    "Which HTML tag defines a paragraph?",
+  choiceA: "Wrong",
+  choiceB: "Wrong",
+  choiceC: "Correct",
+  correct: "C",
+},
+];
+
 startEl.addEventListener("click", function () {
   alert("button clicked");
   console.log(startEl);
 });
-
-
