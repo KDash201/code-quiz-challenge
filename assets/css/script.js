51f3ad696e9e951d6d3f43ea04c8b352f2c48785
KDash201 <KVonMadison@Yahoo.com>
2021-11-21 16:13:11 -0500

    Selected Elements in JS

---
+++ assets/css/script.js
@@ -0,0 +1,15 @@
+// Selected Elements
+const startEl = document.querySelector(".start");
+const headerEl = document.getElementById("header");
+const viewScoreEl = document.getElementById("view-score");
+const timerEl = document.getElementById("timer");
+const containStartEl = document.getElementById("contain-start");
+const questionsEl = document.getElementById("questions");
+const choicesEl = document.getElementById("choices");
+
+startEl.addEventListener("click", function () {
+  alert("button clicked");
+  console.log(startEl);
+});
+
+
