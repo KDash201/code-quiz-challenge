0350e2613629c958a7aaf9355b0b5576f6bfed9f
KDash201 <KVonMadison@Yahoo.com>
2022-03-21 16:51:27 -0400

    refactor askQuestions array

--- assets/css/script.js
+++ assets/css/script.js
@@ -8,46 +8,46 @@ const containStartEl = document.getElementById("contain-start");
 const questionsEl = document.getElementById("questions");
 const choicesEl = document.getElementById("choices");
 
-startEl.addEventListener("click", function () {
-});
-
 // startEl.remove();
 
 // startEl.style.display = "none"
 // containStartEl.style.display = "block"
 
-
 let askQuestions = [
   {
     question:
-      "Which attribute requires you to put a hashtag before the name when deeclaring it in CSS?",
-    choiceA: "Correct",
-    choiceB: "Wrong",
-    choiceC: "Wrong",
+      "Which attribute requires you to put a hashtag before the name when declaring it in CSS?",
+    choiceA: "Id",
+    choiceB: "Class",
+    choiceC: "Href",
     correct: "A",
-  },{
-  question:
-    "How many HTML heading tags are there?",
-  choiceA: "Wrong",
-  choiceB: "Correct",
-  choiceC: "Wrong",
-  correct: "B",
-},{
-  question:
-    "Which HTML tag defines a paragraph?",
-  choiceA: "Wrong",
-  choiceB: "Wrong",
-  choiceC: "Correct",
-  correct: "C",
-},
+  },
+  {
+    question: "How many HTML heading tags are there?",
+    choiceA: "4",
+    choiceB: "6",
+    choiceC: "3",
+    correct: "B",
+  },
+  {
+    question: "Which HTML tag defines a paragraph?",
+    choiceA: "<img>",
+    choiceB: "<ol>",
+    choiceC: "<p>",
+    correct: "C",
+  },
 ];
 
-const showQuestions = function() {
-return askQuestions;
-}
+const showQuestions = function () {
+  return askQuestions;
+};
 
 console.log(askQuestions);
 
-const Quiz = function(askQuestions) {
+const Quiz = function (askQuestions) {};
 
-}
+startEl.addEventListener("click", function () {});
+buttonEl.addEventListener("click", function () {});
+startEl.addEventListener("click", function () {});
+startEl.addEventListener("click", function () {});
+startEl.addEventListener("click", function () {});
