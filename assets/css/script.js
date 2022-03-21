6d6ace3a18b918a0d3cad8fb019f73dc53ffbe07
KDash201 <KVonMadison@Yahoo.com>
2022-03-21 16:36:47 -0400

    hide start quiz button

--- assets/css/script.js
+++ assets/css/script.js
@@ -1,5 +1,5 @@
 // Selected Elements
-const homePageEl = document.querySelector("home-page");
+const homePageEl = document.createElement("div");
 const startEl = document.querySelector(".start");
 const headerEl = document.getElementById("header");
 const viewScoreEl = document.getElementById("view-score");
@@ -8,46 +8,51 @@ const containStartEl = document.getElementById("contain-start");
 const questionsEl = document.getElementById("questions");
 const choicesEl = document.getElementById("choices");
 
-startEl.addEventListener("click", function () {
+
+
+var startQuiz = function() {
+  var startBtn = document.createElement("startButton");
+  startBtn.setAttribute("style", "none");
+};
+
+startQuiz();
+
+
+
+// let askQuestions = [
+//   {
+//     question:
+//       "Which attribute requires you to put a hashtag before the name when deeclaring it in CSS?",
+//     choiceA: "Correct",
+//     choiceB: "Wrong",
+//     choiceC: "Wrong",
+//     correct: "A",
+//   },{
+//   question:
+//     "How many HTML heading tags are there?",
+//   choiceA: "Wrong",
+//   choiceB: "Correct",
+//   choiceC: "Wrong",
+//   correct: "B",
+// },{
+//   question:
+//     "Which HTML tag defines a paragraph?",
+//   choiceA: "Wrong",
+//   choiceB: "Wrong",
+//   choiceC: "Correct",
+//   correct: "C",
+// },
+// ];
+
+// const showQuestions = function() {
+// return askQuestions;
+// }
+
+// console.log(askQuestions);
+
+// const Quiz = function(askQuestions) {
+
+// }
+
+startEl.addEventListener("click", function (startQuiz) {
 });
-
-// startEl.remove();
-
-// startEl.style.display = "none"
-// containStartEl.style.display = "block"
-
-
-let askQuestions = [
-  {
-    question:
-      "Which attribute requires you to put a hashtag before the name when deeclaring it in CSS?",
-    choiceA: "Correct",
-    choiceB: "Wrong",
-    choiceC: "Wrong",
-    correct: "A",
-  },{
-  question:
-    "How many HTML heading tags are there?",
-  choiceA: "Wrong",
-  choiceB: "Correct",
-  choiceC: "Wrong",
-  correct: "B",
-},{
-  question:
-    "Which HTML tag defines a paragraph?",
-  choiceA: "Wrong",
-  choiceB: "Wrong",
-  choiceC: "Correct",
-  correct: "C",
-},
-];
-
-const showQuestions = function() {
-return askQuestions;
-}
-
-console.log(askQuestions);
-
-const Quiz = function(askQuestions) {
-
-}
